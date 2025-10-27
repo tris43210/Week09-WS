776966cba69748c6571b68d5518a1113c7726e74
tris43210 <tristan.g.king@gmail.com>
2025-10-27 11:57:45 +0000

    basic clerk setup

---
+++ 01-Logins-w-Clerk/clerk-logins/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left"></div>
+    </main>
+  );
+}
